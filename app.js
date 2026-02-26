0d577b3f9c4eb9b2f684c3964a357414c2db7353
kaike1505 <kaike.francisco@escola.pr.gov.br>
2026-02-26 10:42:33 -0300

    enviando app.js

---
+++ app.js
@@ -0,0 +1,8 @@
+
+let nomealuno = "Kaike";
+
+let idadealuno = 16;
+
+let estamatriculado = true;
+
+console.log(`Aluno ${nomealuno}, tem ${idadealuno} anos, Status da matrícula: ${estamatriculado}`);
